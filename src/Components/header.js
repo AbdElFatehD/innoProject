64101fa4b9844219512eb03ef14633eec1e8c724
ali_maameri02 <ali.maameri@univ-constanine2.com>
2024-08-28 14:07:02 +0100

    imed commit link login +sign up

--- src/Components/header.js
+++ src/Components/header.js
@@ -2,37 +2,46 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Button from "react-bootstrap/Button";
+import { Link } from 'react-router-dom';
 import "./header.css";
 import logo from './imgs/logo.png'; 
 import flag from './imgs/flag.png';
 import user from './imgs/user.png';
 
-
-function header ( ) {
-    return (
-        <>
-        <div className='container-custome'>
-          <Navbar bg="bg-white" data-bs-theme="dark" className='nav-style  animation'>
-          
-              <Navbar.Brand href="#home" className="text-dark " ><span className='logo-size'><span className='logo-span'><span><img src={logo} /></span>InnoNexus</span> Hub</span></Navbar.Brand>
-              <Nav className="me-auto">
-                
-                    <Nav.Link href="#home" className="text-dark font-size margin-custome list-items hover-items"  > الرئيسية</Nav.Link>
-                    <Nav.Link href="#features" className="text-dark font-size margin-custome list-items hover-items">التحديات</Nav.Link>
-                    <Nav.Link href="#pricing" className="text-dark font-size margin-custome list-items hover-items">شركات</Nav.Link>
-                    <Nav.Link href="#pricing" className="text-dark font-size margin-custome list-items hover-items">الرعاة</Nav.Link>
-                    <Nav.Link href="#pricing" className="text-dark font-size margin-custome list-items hover-items">من نحن</Nav.Link>
-                
-                <div className='right-div'>
-                    <Button href="#" className='btn-light btn-custome   inside-margin' >تسجيل الدخول</Button>
-                    <Button href="#"className='btn-warning btn-custome inside-margin' >  اشتراك </Button>
-                    <span ><img src={user} className='inside-margin' /></span>
-                    <span ><img src={flag}  className='inside-margin' /></span>
-                    </div>
-              </Nav>
-          </Navbar>
+function Header() {
+  return (
+    <>
+      <div className='container-custome'>
+        <Navbar bg="bg-white" data-bs-theme="dark" className='nav-style  animation'>
+          <Navbar.Brand href="#home" className="text-dark">
+            <span className='logo-size'>
+              <span className='logo-span'>
+                <span><img src={logo} alt="Logo" /></span>InnoNexus
+              </span> 
+              Hub
+            </span>
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home" className="text-dark font-size margin-custome list-items hover-items">الرئيسية</Nav.Link>
+            <Nav.Link href="#features" className="text-dark font-size margin-custome list-items hover-items">التحديات</Nav.Link>
+            <Nav.Link href="#pricing" className="text-dark font-size margin-custome list-items hover-items">شركات</Nav.Link>
+            <Nav.Link href="#pricing" className="text-dark font-size margin-custome list-items hover-items">الرعاة</Nav.Link>
+            <Nav.Link href="#pricing" className="text-dark font-size margin-custome list-items hover-items">من نحن</Nav.Link>
+          </Nav>
+          <div className='right-div'>
+            <Link to="/login">
+              <Button className='btn-light btn-custome inside-margin'>تسجيل الدخول</Button>
+            </Link>
+            <Link to="/signup">
+              <Button className='btn-warning btn-custome inside-margin'>اشتراك</Button>
+            </Link>
+            <span><img src={user} className='inside-margin' alt="User" /></span>
+            <span><img src={flag} className='inside-margin' alt="Flag" /></span>
           </div>
-          </>
-      );
+        </Navbar>
+      </div>
+    </>
+  );
 }
-export default header;
+
+export default Header;
